c6d586a41153ac7e43efcc2756298487cf88f071
patrikbraborec <patrikbraborec@gmail.com>
2023-01-06 20:00:00 +0100

    Add chatbot

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
     return (
         <Html>
             <Head>
-                <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="shortcut icon" href="/favicon.ico"/>
                 <title>Colabu - řešení pro správu zboží</title>
             </Head>
             <body>
-            <Main />
-            <NextScript />
+            <Main/>
+            <NextScript/>
+            <script type="text/javascript" id="hs-script-loader" async defer
+                    src="//js-eu1.hs-scripts.com/26710754.js"></script>
             </body>
         </Html>
-    )
+    );
 }
